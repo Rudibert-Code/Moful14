e0ab53629058f11f2838a464a808ba9044a9e189
Bjoern <onoofelogmail.com>
2026-05-11 14:00:49 +0200

    - created tsconfig.json - created "dis" & "src" folder

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+"use strict";
+// .ts = TypeScript
+Object.defineProperty(exports, "__esModule", { value: true });
+let add = (a, b) => { return a + b; };
+// Konsoleneintrag zum Umwandeln in modernes JavaScript : tsc script.ts --target ESNext --module commonjs 
+// Konsoleneintrag zum Erzeugen einer tsconfog.json : tsc --init
---
+++ src/script.ts
@@ -0,0 +1,6 @@
+// .ts = TypeScript
+
+let add = (a:number, b:number) => {return a + b};
+
+// Konsoleneintrag zum Umwandeln in modernes JavaScript : tsc script.ts --target ESNext --module commonjs 
+// Konsoleneintrag zum Erzeugen einer tsconfog.json : tsc --init
