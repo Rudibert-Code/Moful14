3c54d8867d60aaa4528b2d594ae0e9061044d560
Bjoern <onoofelogmail.com>
2026-05-12 14:33:37 +0200

    interfaces deep dive

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,34 @@
 // Um etwas in eine andere Datei importieren zu können, MUSS es mit export freigegeben werden
 export interface User{
-    name:string,
+    readonly name:string, // readonly  = Eigenschaften können nicht geändert werden
     role?:string, // Optionaler Objekt
 }
+
+interface Car{
+    wheels:number,
+}
+
+interface Employee extends User, Car{ // extends User  = Alles was in User (siehe oben) bereits definiert wurde, wird hier automatisch hinzugefügt
+    staff:boolean,
+}
+
+let carl:Employee = {
+    name:"Karl",
+    staff:true,
+    role:"user",
+    wheels:2,
+}
+
+
+
+let sven:Employee = {
+    name:"Karl",
+    staff:true,
+    role:"user",
+    wheels:2,
+    birthday:"monday"
+}
+
+let person:Employee = sven; 
+//  Checkt, ob in sven alle Angaben enthalten sind, die für interface "Employee" benötigt werden, wenn mehr enthalten sind, werden diese nicht als Fehler angezeigt. Dies verursacht allerdings Fehler.
+//  Kann benutzt werden um zu prüfen, ob in einer von einer Datenbank erhaltenen Datei alle nötigen Informationen enthalten sind.
